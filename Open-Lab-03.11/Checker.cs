b655be2600619cdffded755562d53aaaa1e8309c
matejslovak <joggurtik1@gmail.com>
2019-11-11 09:58:10 +0100

    done

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,24 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
-        } 
+            int min = 0;
+            int max = str.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = str[min];
+                char b = str[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
     }
+
 }
